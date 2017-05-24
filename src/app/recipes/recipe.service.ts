68af22e8b3edc203266acd6678a2f5092a7a023a
Marian Marian <maryan27m@gmail.com>
2017-05-24 19:24:22 +0300

    recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit} from '@angular/core';
-import { Recipe} from '../recipe.model';
+import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
-@Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
-})
-export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[] = [
     new Recipe('A test', "desc", 'https://www.google.com.ua/search?q=recipe&client=opera&hs=Y4P&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiZlo2Ig4PUAhXsJ5oKHZAwDm8Q_AUICigB&biw=1366&bih=658#imgrc=TNc-ZvDRzJVP9M:'),
     new Recipe('A test', "desc", 'https://www.google.com.ua/search?q=recipe&client=opera&hs=Y4P&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiZlo2Ig4PUAhXsJ5oKHZAwDm8Q_AUICigB&biw=1366&bih=658#imgrc=TNc-ZvDRzJVP9M:')
   ];
-  constructor() { }
 
-  ngOnInit() {
-  };
+
+  getRecipes(){
+
+  }
 }
