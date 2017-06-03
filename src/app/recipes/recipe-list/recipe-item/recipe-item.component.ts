85fef2d0ec820aa50105295ecc3bc66fc11ea821
Marian Marian <maryan27m@gmail.com>
2017-06-03 16:21:14 +0300

    route children

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,14 +9,15 @@ import {RecipeService} from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit, Input {
   @Input() recipe: Recipe;
+  @Input() index: number;
  
   constructor(private recipeService: RecipeService ) { }
 
   ngOnInit() {
   };
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  // onSelected() {
+  //   this.recipeService.recipeSelected.emit(this.recipe);
+  // }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -17,7 +17,10 @@ export class RecipeService{
 
   constructor(private shoppinglistService: ShoppingListService){}
   getRecipes(){
-
+    return this.recipes.slice();
+  }
+  getRecipe(index: number){
+    return this.recipes[index];
   }
    addIngredientsToShoppingList(ingredients: Ingredient[]){
       this.shoppinglistService.addIngredients(ingredients);
